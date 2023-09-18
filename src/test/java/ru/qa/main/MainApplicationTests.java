7390ac8c73e11d76bc16e51a7d0c3d8329bf5c0a
Nedomets Alexandr <anedomets@yandex.ru>
2023-09-18 13:21:55 +0300

    Initial commit

---
+++ src/test/java/ru/qa/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qa.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
